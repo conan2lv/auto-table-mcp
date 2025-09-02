90cfd4614be974b6cfb283f8e1229e4241a96e3b
lvjunwei <lvjunwei.lvjunwei@alibaba-inc.com>
2025-09-02 11:10:12 +0800

    feat: 增加限制

--- src/tools/generate-auto-table-column.ts
+++ src/tools/generate-auto-table-column.ts
@@ -9,7 +9,12 @@ const registryTool = (server: McpServer) => {
     `根据列信息生成符合 AutoTable 组件规范的columns属性
 适用场景：
 1. 用户给出表格列的信息, 根据列信息生成符合AutoTable组件规范的columns属性
-2. 用户通过上传图片, 根据图片解析出表格列的信息并生成columns属性`,
+2. 用户通过上传图片, 根据图片解析出表格列的信息并生成columns属性
+
+如何使用返回结果
+1. 以当前tool返回的结果构建AutoTable组件的columns属性
+2. 不要篡改返回结果, 不要将其转化为标准的Antd Table格式
+3. 属性尽可能的保留, 并以此为参考构建更适合的columns属性`,
     {
       columns: z
         .array(
